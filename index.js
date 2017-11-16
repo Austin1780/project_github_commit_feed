093cc5361a041f5e7bc6e314b30ac8527d52ffd3
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-16 16:12:20 -0700

    added JSON, parsed URL

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ let http = require("http");
 let express = require("./lib/express.js");
 let router = require("./lib/router");
 let url = require("url");
-
+let querystring = require("querystring");
 
 let app = express();
 let port = process.env.PORT || 3000;
 let host = "localhost";
 
-app.get("/", (req, res) => {
-	var query=req.params;
-	console.log(query);
-  console.log("inside get");
-});
+app.get("/", (req, res) => {});
 
 app.get("/commits", (req, res) => {
-  console.log(url.parse(req.url));
-  console.log("inside get");
+  //let userQuery = url.parse(req.url).query;
+  //console.log("======");
+  //console.log(userQuery);
+  let userQuery = querystring.parse(req.url);
+  let owner = userQuery["/commits?username"];
+  let repo = userQuery["repo"];
+  console.log(owner);
+  console.log(repo);
 
-
-  gitHub.authenticate();
+  /*gitHub.authenticate();
   gitHub
-  .getRepos(owner, repo)
-  .then(function(results) {
-    resultsObject = results;
-  })
-  .then(function() {
-    parseData(resultsObject);
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-
+    .getRepos(owner, repo)
+    .then(function(results) {
+      resultsObject = results;
+    })
+    .then(function() {
+      resultsObject;
+    })
+    .catch(function(err) {
+      console.error(err);
+    });*/
 });
 /*
 app.post('/', (req, res) => {
@@ -46,13 +46,9 @@ app.listen(port, host, () => {
 //const owner = "Austin1780";
 //const repo = "assignment_node_dictionary_reader";
 
-
-
 var resultsObject;
 
-
-
-function parseData(resultsObject) {
+/*function parseData(resultsObject) {
   let sha = [];
   let author = [];
   let url = [];
@@ -66,7 +62,7 @@ function parseData(resultsObject) {
     url[i] = resultsObject.data[i].url;
   }
 
-  //console.log(sha);
-  //console.log(author);
-  //console.log(url);
-}
+  console.log(sha);
+  console.log(author);
+  console.log(url);
+}*/
