bcbfdcd7dbe4ea396c8502b06b2646c70eae7260
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-16 11:06:22 -0700

    working on bootstrap button

--- index.js
+++ index.js
@@ -1,48 +1,41 @@
-let gitHub = require('./lib/getRepos.js');
+let gitHub = require("./lib/getRepos.js");
+let submitRequest = require("./lib/submitRequest.js");
 const owner = "Austin1780";
 const repo = "assignment_node_dictionary_reader";
 
 gitHub.authenticate();
 
+submitRequest();
+
 var resultsObject;
 
-gitHub.getRepos(owner,repo)
+gitHub
+  .getRepos(owner, repo)
   .then(function(results) {
-  
-    resultsObject= results;
+    resultsObject = results;
   })
   .catch(function(err) {
     console.error(err);
   })
   .then(function() {
-  		 
-  		parseData(resultsObject);
-
+    parseData(resultsObject);
   });
 
 function parseData(resultsObject) {
-	let sha = [];
-	let author=[];
-	let url = [];
+  let sha = [];
+  let author = [];
+  let url = [];
 
-	for(i=0;i<resultsObject.data.length; i++)
-	{
+  for (i = 0; i < resultsObject.data.length; i++) {
+    //console.log(resultsObject.data[i].sha);
+    sha[i] = resultsObject.data[i].sha;
+    //console.log(resultsObject.data[i].commit.author);
+    author[i] = resultsObject.data[i].commit.author;
+    //console.log(resultsObject.data[i].url);
+    url[i] = resultsObject.data[i].url;
+  }
 
-		//console.log(resultsObject.data[i].sha);
-		sha[i]=resultsObject.data[i].sha;
-		//console.log(resultsObject.data[i].commit.author);
-		author[i]=resultsObject.data[i].commit.author;
-		//console.log(resultsObject.data[i].url);
-		url[i]=resultsObject.data[i].url;
-	}
-
-	console.log(sha);
-	console.log(author);
-	console.log(url);
-	
-
-	var currentSha;
-
+  console.log(sha);
+  console.log(author);
+  console.log(url);
 }
- 
-
