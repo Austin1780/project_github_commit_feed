cbc40d5d6c7fbc066014c7946ca3e55f07b723ec
triflegated <adatepe.justin@gmail.com>
2017-11-16 11:37:36 -0500

    wrapper done testing auth

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-let Github = require('github');
+let getRepos = require('./lib/getRepos.js');
+const owner = "austin1780";
+const repo = "project_github_commit_feed"
 
-let github = new Github({
+var resultGetRepos= getRepos.pull(owner,repo);
+console.log("resultGetRepos: \n"+resultGetRepos);
 
-})
 
-github.authenticate({
-    type: "token",
-    token: "c28f65a3098ddf26b564de404d72476051d077be"
-});
-
-let getCommits = (owner, repo) => github.repos.getCommits({
-  owner,
-  repo
-});
-
-getCommits("austin1780", "assignment_building_the_express_router")
-  .then(function(results) {
-    console.log(results);
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-
+/*
 console.log("original getCommits: ");
 console.log(getCommits);
+*/
