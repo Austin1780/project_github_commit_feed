babb28facb756aad701f25c6b56811fba381dd97
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-16 10:11:46 -0700

    working on parsing data

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-let getRepos = require('./lib/getRepos.js');
-const owner = "austin1780";
-const repo = "project_github_commit_feed"
+let gitHub = require('./lib/getRepos.js');
+const owner = "Austin1780";
+const repo = "assignment_node_dictionary_reader";
 
-var resultGetRepos= getRepos.pull(owner,repo);
-console.log("resultGetRepos: \n"+resultGetRepos);
+gitHub.authenticate();
+
+let results = gitHub.getRepos(owner,repo)
+  .then(function(results) {
+    return results;
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+
+let sha = results[0].data[0]
+
+
+let url
+let author
+
 
 
 /*
